18289ec094a589b2a1c874e7f7d7a0cc9babef25
khaled elkhouja <122677498+khaledelkhouja@users.noreply.github.com>
2023-04-01 18:42:04 +0200

    allow grid size selection

--- scrip.js
+++ scrip.js
@@ -1,17 +1,40 @@
+ //initial grid 16*16
+ const container = document.getElementsByClassName("container");
+let gridSize=16;                               
+for (let i=0; i<256;i++) addSquare();
+
+// button set grid size
+let btn = document.getElementById("btn");
+btn.addEventListener("click", createGrid );
+
+// grid Construction
 function addSquare() {
-    const container = document.getElementsByClassName("container");
-
-    var square = document.createElement("div");
+    
+    let square = document.createElement("div");
     square.classList.add("grid-square")
-    console.log(square);
     container[0].appendChild(square);
-    square.addEventListener("mouseenter",()=>square.style.backgroundColor= "orange");
+    side=320/gridSize;
+    square.style.width= side +"px";
+    square.style.height= side +"px";
+    //set event that permanently change background color on mouse enter
+    square.addEventListener("mouseenter",()=>square.style.backgroundColor= "#1f2937");
 }
 
-for (let i=0; i<256;i++) {
-    addSquare()
+// 
+function createGrid() {
+    //remove existing grid
+    const grid = document.getElementById("container");
+    grid.replaceChildren();
+    //ask for grid size
+    gridSize=Math.round(prompt("Set grid size","16"));
+    // set grid size limit
+    if (gridSize<=64 && gridSize>0){
+        for (let i=0; i<Math.pow(gridSize,2) ;i++) {
+            addSquare()
+        }  
+    } else {
+        alert("Enter a number between 1 and 64");
+        gridSize=16;
+        for (let i=0; i<256;i++) addSquare();
+    }
 }
-
-
-
-
